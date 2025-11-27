b6782312b420d1cb39dcabdb2549e03e1eb883bf
KrupankD <krupankdanappa@gmail.com>
2025-11-27 14:45:29 +0530

    Add files via upload

---
+++ BinarySearchtree.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+
+};
+typedef struct node *NODE;
+
+NODE createbst(NODE root,int key)
+{
+    NODE temp,cur,prev;
+    temp=(NODE)malloc(sizeof(struct node));
+    temp->data=key;
+    temp->right=NULL;
+    temp->left=NULL;
+    if(root==NULL)
+        return temp;
+    cur=root;
+    prev=NULL;
+    while(cur!= NULL)
+    {
+        prev=cur;
+        if(key<cur->data)
+            cur=cur->left;
+        else
+            cur=cur->right;
+
+    }
+    if(key<prev->data)
+        prev->left=temp;
+    else
+        prev->right=temp;
+    return root;
+}
+
+void inorder(NODE root)
+{
+    if(root!=NULL)
+    {
+        inorder(root->left);
+        printf("%d\t",root->data);
+        inorder(root->right);
+
+    }
+}
+
+void preorder(NODE root)
+{
+    if(root!=NULL)
+    {
+
+        printf("%d\t",root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(NODE root)
+{
+    if(root!=NULL)
+    {
+
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d\t",root->data);
+    }
+}
+
+int main()
+{
+   NODE root=NULL;
+   int item,ch;
+   for(;;)
+   {
+       printf("\n 1.Insert");
+       printf("\n 2.Preorder");
+       printf("\n 3.Inorder");
+       printf("\n 4.Postorder");
+       printf("\n Read choice");
+       scanf("%d",&ch);
+
+       switch(ch)
+       {
+           case 1 : printf("read item to be inserted");
+                    scanf("%d",&item);
+                    root=createbst(root,item);
+                    break;
+
+           case 2 : printf("\n The preorder traversal is\n");
+                    preorder(root);
+                    break;
+
+           case 3 : printf("\n The Inorder traversal is\n");
+                    inorder(root);
+                    break;
+
+           case 4 : printf("\n The postorder traversal is\n");
+                    postorder(root);
+                    break;
+
+           default : exit(0);
+       }
+   }
+   return 0;
+}
